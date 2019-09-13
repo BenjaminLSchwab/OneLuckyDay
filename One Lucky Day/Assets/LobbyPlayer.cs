a663abfcffa5bf56cf4545a2278aacdcf1e0a4ad
Ben <benjamin.schwab.nonspam@gmail.com>
2019-09-13 11:46:54 -0700

    basic lobby

---
+++ One Lucky Day/Assets/LobbyPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyPlayer : MonoBehaviour
+{
+    private string selectedGame = "";
+    Rigidbody2D rb;
+    [SerializeField] float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = new Vector3(Input.GetAxis("Horizontal"), 0);
+        rb.velocity = force * speed;
+    }
+
+
+
+}
