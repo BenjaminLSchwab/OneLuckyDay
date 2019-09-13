6ba58b1c7585ffe20101b1c08431b05a44a29343
Ben <benjamin.schwab.nonspam@gmail.com>
2019-09-13 16:35:35 -0700

    art for darts game

--- One Lucky Day/Assets/Scripts/GameManager.cs
+++ One Lucky Day/Assets/Scripts/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this);
         playerMoney = playerStartingMoney;
     }
 
