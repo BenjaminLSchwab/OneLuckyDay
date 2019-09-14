b9cfbb535bb618d2f150eca6f1149a2ad38d9fc5
Ben <benjamin.schwab.nonspam@gmail.com>
2019-09-13 17:31:14 -0700

    darts target system

---
+++ One Lucky Day/Assets/Scripts/Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    GameObject path;
+    float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path)
+        {
+            
+        }
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public void SetPath(GameObject newPath)
+    {
+        path = newPath;
+    }
+}
---
+++ One Lucky Day/Assets/Scripts/TargetManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    [SerializeField] GameObject path;
+    [SerializeField] List<GameObject> targets;
+    [SerializeField] float targetSpeed = 1f;
+    [SerializeField] float targetSpacing = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Begin()
+    {
+
+    }
+
+    IEnumerator SpawnLoop()
+    {
+        for (int i = 0; i < targets.Count; i++)
+        {
+
+            var target = Instantiate(targets[i], transform.position, Quaternion.identity);
+            target.SendMessage("SetSpeed", targetSpeed);
+            target.SendMessage("SetPath", path);
+            yield return new WaitForSeconds(targetSpacing);
+        }
+
+    }
+}
