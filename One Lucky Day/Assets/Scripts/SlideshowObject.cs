925f6c296146cab0d6651c69f96faba2bef2301c
Ben <benjamin.schwab.nonspam@gmail.com>
2019-09-18 14:42:47 -0700

    add intro sequence

---
+++ One Lucky Day/Assets/Scripts/SlideshowObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SlideshowObject : MonoBehaviour
+{
+    [SerializeField] float time = 5f;
+    [SerializeField] GameObject nextObject;
+    [SerializeField] bool loadLobbyWhenDone = false;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            if (nextObject)
+            {
+                nextObject.SetActive(true);
+            }
+            if (loadLobbyWhenDone)
+            {
+                SceneManager.LoadScene("Lobby");
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
